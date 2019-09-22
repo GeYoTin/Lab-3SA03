2c35a444febe637c4f4e0a21923f780a82384981
GeYoTin <47670687+GeYoTin@users.noreply.github.com>
2019-09-22 19:44:25 +0700

    More

--- App.js
+++ App.js
@@ -1,15 +1,12 @@
 import React, { Component } from 'react';
-import CharacterCard from './CharacterCard'
+import WordCard from './WordCard'
 import './App.css';
 
-const word = "hello"
 class App extends Component{
   render() {
     return (
       <div className="App">
-        {
-          Array.from(word).map((c,i) => <CharacterCard value={c} key={i}/>)
-        }
+        <WordCard value="hello"/>
       </div>
     );
   }
