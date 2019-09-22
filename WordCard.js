63eed5f5d42af76721bd11b1acf87575490f7b73
GeYoTin <47670687+GeYoTin@users.noreply.github.com>
2019-09-22 20:01:19 +0700

    Game Logic : แสดงรอบ

--- WordCard.js
+++ WordCard.js
@@ -36,6 +36,7 @@ export default class WordCard extends Component {
     render() {
         return (
             <div>
+                <p>ROUND : {this.state.attempt}</p>
                 {
                     Array.from(this.state.chars).map((c, i) => 
                     <CharacterCard value={c} key={i} attempt={this.state.attempt} activationHandler={this.activationHandler}/>)
