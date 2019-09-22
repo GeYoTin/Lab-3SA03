b4cc7ae285f5cf246dedc286d9337b898af36db7
GeYoTin <47670687+GeYoTin@users.noreply.github.com>
2019-09-22 19:36:36 +0700

    Components & Props : CharacterCard

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import CharacterCard from './CharacterCard'
 import './App.css';
 
 class App extends Component{
   render() {
     return (
       <div className="App">
-        Hello {"World"}
+        <CharacterCard value="h"/>
+        <CharacterCard value="i"/>
       </div>
     );
   }
